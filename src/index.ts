991ad87c02e99bde7adbc577f41a96fd57198bc9
李瑞清 <alger_li@163.com>
2021-12-24 17:17:46 +0800

    docs: api文档

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@
  * @Author: OBKoro1
  * @Date: 2021-12-15 16:15:03
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-12-16 11:27:00
+ * @LastEditTime: 2021-12-24 10:02:10
  * @FilePath: \q-utils\src\index.ts
  * @Description: 
  */
 export * from "./dateUtil"
 export * from "./color"
+export * from "./fileUtil"
