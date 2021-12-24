a50ad99b7532c21a76794502cb440099f713cdfe
李瑞清 <alger_li@163.com>
2021-12-24 09:57:43 +0800

    feat: 增加文件工具函数

---
+++ src/fileUtil.ts
@@ -0,0 +1,54 @@
+/*
+ * @Author: OBKoro1
+ * @Date: 2021-12-24 09:50:11
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-12-24 09:56:07
+ * @FilePath: \q-utils\src\fileUtil.ts
+ * @Description: 文件工具函数
+ */
+
+/**
+ * 生成唯一文件名
+ * @param len  生成的文件名长度
+ * @param radix  指定基数
+ * @returns
+ */
+export function getuuid(len: number, radix?:number) {
+  const chars =
+    "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".split("")
+  let uuid = []
+  let i = 0
+  radix = radix || chars.length
+
+  if (len) {
+    for (i = 0; i < len; i++) uuid[i] = chars[0 | (Math.random() * radix)]
+  } else {
+    let r
+    uuid[8] = uuid[13] = uuid[18] = uuid[23] = "-"
+    uuid[14] = "4"
+
+    for (i = 0; i < 36; i++) {
+      if (!uuid[i]) {
+        r = 0 | (Math.random() * 16)
+        uuid[i] = chars[i === 19 ? (r & 0x3) | 0x8 : r]
+      }
+    }
+  }
+
+  return uuid.join("")
+}
+
+/**
+ * 生成唯一文件名 时间戳 + 随机数
+ * @param  len 生成的文件名长度
+ * @param  radix 指定基数
+ */
+export function getTimeId(len: number, radix?: number) {
+  if (len) {
+    const time = new Date().getTime()
+    const uuid = getuuid(len, radix)
+    return `${time}${uuid}`
+  } else {
+    console.log("请输入长度")
+  }
+}
---
+++ test/fileUtils.test.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: OBKoro1
+ * @Date: 2021-12-24 09:53:25
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-12-24 09:56:31
+ * @FilePath: \q-utils\test\fileUtils.test.ts
+ * @Description:
+ */
+import { getuuid, getTimeId } from "../src/fileUtil"
+
+describe("文件工具函数", () => {
+//   test("生成唯一文件名", () => {
+//     expect(getuuid(10)).toBe("1")
+//   })
+//   test("生成唯一文件名根据时间戳", () => {
+//     expect(getTimeId(10)).toBe("1")
+//   })
+})
