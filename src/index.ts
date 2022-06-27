bf7cfd6cfead18034aecbea59f9d495edce696f3
李瑞清 <alger_li@163.com>
2022-06-27 14:33:42 +0800

    docs: api文档

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@
  * @Author: lrqnew
  * @Date: 2021-12-15 16:15:03
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2022-01-07 17:13:06
- * @FilePath: \q-utils\src\index.ts
+ * @LastEditTime: 2022-06-27 14:32:49
+ * @FilePath: \utils-q\src\index.ts
  * @Description: 
  */
 export * from "./dateUtil"
 export * from "./color"
 export * from "./fileUtil"
 export * from "./hex"
+export * from './array'
+export * from './base64'
